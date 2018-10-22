1e2a0ba49ac3159b8926c7f8f8ccf9906b01a924
Yangzhedi <uiryzd@163.com>
2018-10-22 20:42:15 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
+
 import logo from '@/assets/logo.svg'
 import Login from '@/routes/Test'
-import { Steps, Icon } from 'antd'
+
 import './App.css'
 
-const { Step } = Steps
 
 class App extends Component {
   render() {
@@ -14,15 +14,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to bangbisai!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edi132t <code>src/App.js</code> and save to reload.
-        </p>
-        <Steps>
-          <Step status="finish" title="Login" icon={<Icon type="user" />} />
-          <Step status="finish" title="Verification" icon={<Icon type="solution" />} />
-          <Step status="process" title="Pay" icon={<Icon type="loading" />} />
-          <Step status="wait" title="Done" icon={<Icon type="smile-o" />} />
-        </Steps>
         <Login />
       </div>
     )
